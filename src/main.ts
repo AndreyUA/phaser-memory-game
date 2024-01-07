1e490c3f122774289fdd2363066b91c3bc0bae22
AndreyUA <infernalsteel666@gmail.com>
2024-01-07 21:37:26 +0200

    implemented empty scene

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import "./style.css";
 
+import * as Phaser from "phaser";
+
 console.log("test log");
+
+const scene: Phaser.Types.Scenes.SceneType = new Phaser.Scene("main");
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  scene,
+};
+
+new Phaser.Game(config);
